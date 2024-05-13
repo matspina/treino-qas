eb9c4a658b1eaee7a1c699057ce99b5036c432cc
Matheus Spina Polito <mspina@ciandt.com>
2024-05-13 15:58:40 -0300

    Adding first class

---
+++ aula-01/test-01.js
@@ -0,0 +1,21 @@
+// Para comentar várias linhas com um atalho no VS code, se usa: command/control + '/'
+
+const test1 = 'test 1'
+
+let test2 = 'test'
+
+test2 = 'test 2'
+
+console.log(test1 + test2)
+
+const x = 4
+const y = 5
+
+// Esperado que faça soma
+console.log(x + y)
+
+const serviceUrl = 'https://test.api.bla.com'
+const stringExample = `POST: ${serviceUrl} `
+
+// esperado que logue junto com o service url
+console.log(stringExample)
