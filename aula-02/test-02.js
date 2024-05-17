c7aafbc06c25816d03f0295d9261c219943b07bb
oarthurag <arthurag@ciandt.com>
2024-05-17 15:29:03 -0300

    aula 2 treino qas

---
+++ aula-02/test-02.js
@@ -0,0 +1,44 @@
+let peso = 100;
+
+let altura = 2;
+
+
+
+/*
+peso/altura*altura
+
+**
+*
+
+*/
+
+//console.log(peso/(altura*altura))
+
+altura *= altura // altura = altura * altura
+// -= +=
+// % 
+// * / 
+
+//console.log(altura)
+
+//console.log(altura**2)
+
+
+// tipos primitivos - String Number Boolean Null Undefined
+
+let String = "Nome"
+let number = 89
+let bool = true
+let undefined;
+let nula = null
+
+//console.log(typeof bool, bool)
+
+// incremento e decremento (++ , --)
+// ++ => number = number + 1
+// -- => number = number - 1
+console.log(++number)
+console.log(number)
+
+console.log(number--)
+console.log(number)
